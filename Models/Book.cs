77f610bd30b12e045ab3c1905db89bef2f166037
Mohamed Ashraf Mohamed <43254148+MAbuElmagd@users.noreply.github.com>
2019-08-11 01:06:26 +0200

    Add files via upload

---
+++ Models/Book.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Books_Recommendation_System.Models
+{
+    public class Book
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public DateTime PublishYear { get; set; }
+        public string Categories { get; set; }
+        public string BookLength { get; set; }
+        public string Language { get; set; }
+        public string Author { get; set; }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+namespace Books_Recommendation_System.Models
+{
+    public class User
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Password { get; set; }
+        public int Age { get; set; }
+        public string PreferCatg { get; set; }
+        public string BookLength { get; set; }
+        public string Language { get; set; }
+
+    }
+}
